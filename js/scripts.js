528339b9d929a72d6c0d944e8fcdfdae7adfe31a
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-07-11 12:47:21 +0300

    js files commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,55 @@
-var add = function(number1,number2){return number1 + number2;};
+var add= function(number1,number2){return number1 + number2;};
+
+var subtract= function(number1,number2){return number1 - number2;};
+
+var multiply= function(number1,number2){return number1 * number2;};
+
+var divide= function(number1,number2){return number1 / number2;};
+
 
 
 var number1=parseInt(prompt("Enter a number:"));
 var number2=parseInt(prompt("Enter another number:"));
 alert(add(number1,number2));
+
+$(index).ready(function add(){
+  var input1 = #no1.val()
+  var input2 = #no2.val()
+});
+
+$(index).ready(function subtract() {
+  var input1 = #no1.val()
+  var input2 = #no2.val()
+});
+
+$(index).ready(function multiply() {
+var input1 = #no1.val()
+var input2 = #no2.val()
+});
+
+$(index).ready(function divide() {
+  var input1 = #no1.val()
+  var input2 = #no2.val()
+});
+
+
+
+  if select === add
+else if (true) {
+
+}if select===subtract{
+
+} else {
+
+}if select === multiply{
+
+} else {
+
+}if select === divide {
+
+} else {
+
+}
+
+
+event.preventDefault
